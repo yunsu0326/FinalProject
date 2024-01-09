fccef97af4ca2ddbd2f524d78c927a2802aa14ab
yosub <lee.yo.sub94@gmail.com>
2024-01-09 16:54:25 +0900

    index

--- FInalProject/src/main/java/com/spring/app/yosub/model/YosubDAO.java
+++ FInalProject/src/main/java/com/spring/app/yosub/model/YosubDAO.java
@@ -7,6 +7,7 @@ import java.util.Map;
 
 import com.spring.app.digitalmail.domain.EmailVO;
 import com.spring.app.domain.Calendar_schedule_VO;
+import com.spring.app.domain.DraftVO;
 import com.spring.app.domain.EmployeesVO;
 
 public interface YosubDAO {
@@ -23,6 +24,10 @@ public interface YosubDAO {
 	List<Calendar_schedule_VO> scheduleselect(Map<String, Object> paraMap);
 
 	List<EmailVO> SelectMyEmail_withPaging(Map<String, Object> paraMap);
+
+	List<DraftVO> getMyDraftProcessing(String employee_id);
+
+	List<DraftVO> getMyDraftProcessed(String employee_id);
 
 	
 	
